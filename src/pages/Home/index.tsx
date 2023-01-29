5ef1c9c3b3ff2c5f9eb327b1292c408f472d1a01
leek-emperor <1332590314@qq.com>
2023-01-29 20:22:13 +0800

    feat: 初始化

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,23 @@
+const Home = () => {
+	return (
+		<div>
+			<h1>hello-ssr</h1>
+			<button
+				onClick={(): void => {
+					alert('hello-ssr');
+				}}
+			>
+				alert
+			</button>
+			<button
+				onClick={(): void => {
+					alert('hello-ssr');
+				}}
+			>
+				alert
+			</button>
+		</div>
+	);
+};
+
+export default Home;
